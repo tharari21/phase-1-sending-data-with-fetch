447265ed647ed1e4c3199382297dbb8da93ba6b9
Tomer Harari <tharari21@gmail.com>
2022-07-08 16:02:58 -0400

    commit tree b79493dd5898aa87f28da4357d5c8335d16dc406

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Add your code here
+const submitData = (userName, userEmail) => {
+  const newUser = {
+    name: userName,
+    email: userEmail,
+  };
+  const options = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify(newUser),
+  };
+  return fetch("http://localhost:3000/users", options)
+    .then((res) => res.json())
+    .then((user) => {
+      console.log(user);
+      document.body.append(user.id);
+    })
+    .catch((err) => {
+      document.body.append(err.message);
+    });
+};
+submitData("Tomer", "tharari21@gmail.com");
